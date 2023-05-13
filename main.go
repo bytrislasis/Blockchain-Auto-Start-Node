945c0dbe703f3b5f31690d8e91b535f258c9845a
sbr <mysql_query@hotmail.com.tr>
2023-05-13 23:12:17 +0300

    private chain start config.json eklendi

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/ecdsa"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -16,9 +18,56 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 )
 
+type Config struct {
+	Period  int64 `json:"period"`
+	ChainId int64 `json:"chainId"`
+}
+
+func readConfig() Config {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config Config
+	if err = json.Unmarshal(bytes, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	return config
+}
+
+func writeInfoFile(nodes []string, keys []*ecdsa.PrivateKey) {
+	file, err := os.Create("info.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	for i, _ := range nodes {
+		privateKeyBytes := crypto.FromECDSA(keys[i])
+		privateKey := hex.EncodeToString(privateKeyBytes)
+		address := crypto.PubkeyToAddress(keys[i].PublicKey)
+
+		_, err := file.WriteString(fmt.Sprintf("address: %s\nprivatekey: %s\npassword: asdasdasd\n\n", address.Hex(), privateKey))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
 func main() {
+	config := readConfig()
 	nodes := []string{"node1", "node2", "node3"}
+	var keys []*ecdsa.PrivateKey
 	var addresses []common.Address
+
 	for _, node := range nodes {
 		os.MkdirAll(node, os.ModePerm)
 		passwordFile, err := os.Create(fmt.Sprintf("%s/password.txt", node))
@@ -32,37 +81,45 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		keys = append(keys, key)
+
 		ks := keystore.NewKeyStore(fmt.Sprintf("%s/keystore", node), keystore.StandardScryptN, keystore.StandardScryptP)
 		account, err := ks.ImportECDSA(key, "asdasdasd")
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		// Export private key
+		addresses = append(addresses, account.Address)
+
+		// Save the private key to a file
 		privateKeyBytes := crypto.FromECDSA(key)
-		err = ioutil.WriteFile(fmt.Sprintf("%s/keystore/privatekey.txt", node), []byte(hex.EncodeToString(privateKeyBytes)), 0644)
+		err = ioutil.WriteFile(fmt.Sprintf("%s/keystore/privatekey.txt", node), privateKeyBytes, 0644)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		addresses = append(addresses, account.Address)
 	}
 
-	extraData := make([]byte, 32+len(addresses)*common.AddressLength)
+	extraVanity := 32 // ExtraData field's vanity bytes size
+	extraSeal := 65   // ExtraData field's seal bytes size
+
+	// Prepare extraData field content
+	extraData := make([]byte, extraVanity+len(addresses)*common.AddressLength+extraSeal)
 	for i, validator := range addresses {
-		copy(extraData[32+i*common.AddressLength:], validator[:])
+		copy(extraData[extraVanity+i*common.AddressLength:], validator[:])
 	}
 
-	// Genesis dosyası oluştur ve init et
-	alloc := make(map[common.Address]core.GenesisAccount)
-	for _, addr := range addresses {
-		alloc[addr] = core.GenesisAccount{Balance: big.NewInt(0).Exp(big.NewInt(10), big.NewInt(18), nil)} // 10^18 is 1 ETH
-	}
 	genesis := &core.Genesis{
 		Config: &params.ChainConfig{
-			// Clique config
+			ChainID:             big.NewInt(config.ChainId),
+			HomesteadBlock:      big.NewInt(0),
+			EIP150Block:         big.NewInt(0),
+			EIP155Block:         big.NewInt(0),
+			EIP158Block:         big.NewInt(0),
+			ByzantiumBlock:      big.NewInt(0),
+			ConstantinopleBlock: big.NewInt(0),
+			PetersburgBlock:     big.NewInt(0),
 			Clique: &params.CliqueConfig{
-				Period: 15,
+				Period: uint64(config.Period),
 				Epoch:  30000,
 			},
 		},
@@ -70,26 +127,35 @@ func main() {
 		Timestamp:  0x0,
 		ExtraData:  extraData,
 		GasLimit:   0x1000000,
-		Difficulty: big.NewInt(1), // Difficulty should be of type *big.Int
-		Alloc:      alloc,
+		Difficulty: big.NewInt(1),
+		Alloc: map[common.Address]core.GenesisAccount{
+			addresses[0]: {Balance: big.NewInt(0).Mul(big.NewInt(1e9), big.NewInt(1e18))}, // 1B ETH
+			addresses[1]: {Balance: big.NewInt(0).Mul(big.NewInt(1e9), big.NewInt(1e18))}, // 1B ETH
+			addresses[2]: {Balance: big.NewInt(0).Mul(big.NewInt(1e9), big.NewInt(1e18))}, // 1B ETH
+		},
 	}
 
 	for _, node := range nodes {
-		// Genesis dosyasını her bir düğümde başlat
-		genesisBytes, err := genesis.MarshalJSON()
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = ioutil.WriteFile(fmt.Sprintf("%s/genesis.json", node), genesisBytes, 0644)
+		// Marshal the genesis to JSON format
+		genesisJSON, err := genesis.MarshalJSON()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		cmd := exec.Command("geth", "--datadir", fmt.Sprintf("./%s", node), "init", fmt.Sprintf("./%s/genesis.json", node))
-		err = cmd.Run()
+		// Write the genesis to a JSON file
+		err = ioutil.WriteFile(fmt.Sprintf("%s/genesis.json", node), genesisJSON, 0644)
 		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Initialize the genesis block
+		cmd := exec.Command("geth", "--datadir", fmt.Sprintf("./%s", node), "init", fmt.Sprintf("./%s/genesis.json", node))
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			// Log the output of the command, this will help us understand the issue
+			log.Printf("geth init output: %s", output)
 			log.Fatal(err)
 		}
 	}
-
+	writeInfoFile(nodes, keys)
 }
